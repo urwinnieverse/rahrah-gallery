0bc116e151de663f3fc89f583046b079d13e99d2
Winnie A <191404552+urwinnieverse@users.noreply.github.com>
2026-06-24 23:04:38 +0300

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,42 @@
+// Function to show a specific pop-up
+function showPopup(popupId) {
+    document.getElementById(popupId).style.display = "flex";
+}
+
+// Function to close a specific pop-up
+function closePopup(popupId) {
+    document.getElementById(popupId).style.display = "none";
+}
+
+// Close pop-up when clicking outside of it
+window.addEventListener("click", function (event) {
+    let popups = document.querySelectorAll(".popup");
+    popups.forEach(popup => {
+        if (event.target === popup) {
+            popup.style.display = "none";
+        }
+    });
+});
+
+let currentShift = 0;
+
+function goRight() {
+    currentShift -= 1130;
+    const paintings = document.querySelectorAll(".paintingsholder");
+    
+    paintings.forEach(painting => {
+        painting.style.transform = `translateX(${currentShift}px)`;
+        painting.style.transition = "transform 2s ease-in-out"; // Makes it smooth!
+    });
+}
+
+
+function goLeft() {
+    currentShift += 1130; 
+    const paintings = document.querySelectorAll(".paintingsholder");
+    
+    paintings.forEach(painting => {
+        painting.style.transform = `translateX(${currentShift}px)`;
+        painting.style.transition = "transform 2s ease-in-out"; // Makes it smooth!
+    });
+}
